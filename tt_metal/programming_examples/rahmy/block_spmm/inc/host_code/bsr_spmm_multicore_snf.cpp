6aa12b4de2f7cb421a72d8799c6e80e6381e647c
Rahmy Salman <rahmysalman@gmail.com>
2026-01-15 17:59:37 +0000

    cleaning up analysis tools

---
+++ tt_metal/programming_examples/rahmy/block_spmm/inc/host_code/bsr_spmm_multicore_snf.cpp
@@ -0,0 +1,7 @@
+// simplest thing we can make: take the sparse_mcast host code and fill it in with SnF kernels
+//     ignore transposing
+
+// if we adapt it from the tt metal example, 
+//     it's all the same except we transpose
+
+// yeah it's probably worth transposing.

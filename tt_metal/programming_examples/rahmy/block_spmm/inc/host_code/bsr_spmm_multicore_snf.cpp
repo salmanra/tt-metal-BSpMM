f3112e3d503631d995b9239e69075a713083e738
Rahmy Salman <rahmysalman@gmail.com>
2026-01-15 22:10:07 +0000

    scaffolding for first store-and-forward implementation

--- tt_metal/programming_examples/rahmy/block_spmm/inc/host_code/bsr_spmm_multicore_snf.cpp
+++ tt_metal/programming_examples/rahmy/block_spmm/inc/host_code/bsr_spmm_multicore_snf.cpp
@@ -4,4 +4,263 @@
 // if we adapt it from the tt metal example, 
 //     it's all the same except we transpose
 
-// yeah it's probably worth transposing.
+// yeah it's probably worth transposing. 
+// Still just SnF on the sparse matrix tho
+
+
+#include "../host_code.hpp"
+
+namespace bsr_host_code{
+
+void bsr_spmm_multicore_snf(
+    bsr_matrix<bfloat16>& a,
+    dense_matrix<bfloat16>& b,
+    dense_matrix<bfloat16>& output,
+    bool bcast_batch,
+    uint32_t nnz_blocks,
+    uint32_t M,
+    uint32_t N,
+    uint32_t K,
+    uint32_t R,
+    uint32_t C,
+    uint32_t B,
+    IDevice* device,
+    bool verbose){
+    // load balanced plus store-and-forwarding for sharing blocks of sparse matrix across core rows
+
+    /// Transposition step:
+    auto small_input_noc = tt::tt_metal::detail::preferred_noc_for_dram_write(device->arch());
+    auto small_input_risc = tt::tt_metal::DataMovementProcessor::RISCV_1;
+    auto large_input_noc = tt::tt_metal::detail::preferred_noc_for_dram_read(device->arch());
+    auto large_input_risc = tt::tt_metal::DataMovementProcessor::RISCV_0;
+
+    // Transpose core grid if the output is wide (M > N)
+    // If transpose core grid, we parallelize M on cores_x and N on cores_y and swap the NOCs and RISCVs
+    // TODO: base the transposition off of the block rizes (R, C, K, N) instead of (M, N)
+    bool transpose_core_grid = M > N;
+
+    auto in0_noc = transpose_core_grid ? large_input_noc : small_input_noc;
+    auto in0_risc = transpose_core_grid ? large_input_risc : small_input_risc;
+    uint32_t in0_parallel_axis_cores = transpose_core_grid ? grid_size.x : grid_size.y;
+
+    auto in1_noc = transpose_core_grid ? small_input_noc : large_input_noc;
+    auto in1_risc = transpose_core_grid ? small_input_risc : large_input_risc;
+    uint32_t in1_parallel_axis_cores = transpose_core_grid ? grid_size.y : grid_size.x;
+    
+
+    CommandQueue& cq = device->command_queue();
+    Program program{};
+
+    tt::DataFormat cb_data_format = tt::DataFormat::Float16_b;
+    MathFidelity math_fidelity = MathFidelity::HiFi4;
+    uint32_t single_tile_size = detail::TileSize(cb_data_format);
+
+    tt::DataFormat indexing_data_format = tt::DataFormat::Int32;
+    uint32_t indexing_data_single_tile_size = detail::TileSize(indexing_data_format);
+    uint32_t num_tiles_for_col_indices = (indexing_data_single_tile_size - 1 + sizeof(int) * nnz_blocks) / indexing_data_single_tile_size;
+    uint32_t num_tiles_for_indptr = (indexing_data_single_tile_size - 1 + sizeof(int) * (M / R + 1)) / indexing_data_single_tile_size;
+    uint32_t num_tiles_indexing = num_tiles_for_col_indices + num_tiles_for_indptr;
+
+    // Core Grid detection
+    auto compute_with_storage_grid_size = device->compute_with_storage_grid_size();
+    // auto compute_with_storage_grid_size = CoreCoord(3, 1);
+    uint32_t num_cores_x = compute_with_storage_grid_size.x;
+    uint32_t num_cores_y = compute_with_storage_grid_size.y;
+    uint32_t num_cores_total = num_cores_x * num_cores_y;
+
+
+
+    // Per-core tiling and blocking args
+    uint32_t Mt = M / TILE_HEIGHT;
+    uint32_t Kt = K / TILE_WIDTH;
+    uint32_t Nt = N / TILE_WIDTH;
+
+    uint32_t Rt = R / TILE_HEIGHT;
+    uint32_t Ct = C / TILE_WIDTH;
+
+    uint32_t in0_block_h = Rt;
+    uint32_t in0_block_w = Ct;
+    uint32_t in1_block_w = get_Npc_from_BSR_block_size(Nt, in0_block_h, in0_block_w, num_cores_x, num_tiles_indexing);
+
+    TT_ASSERT(Mt % in0_block_h == 0);
+    TT_ASSERT(Nt % in1_block_w == 0);
+    TT_ASSERT(Kt % in0_block_w == 0);
+
+    // Core grid assignment
+    std::deque<uint32_t> folded_bsr_matrix_indices;
+    uint32_t nnz_rows = 0;
+    uint32_t folded_index = 0;
+    for (uint32_t i = 0; i < a.indptr.size() - 1; i++) {
+        if (a.indptr[i+1] - a.indptr[i] > 0){
+            folded_bsr_matrix_indices.push_back(folded_index);
+            nnz_rows++;
+        }
+        folded_index++;
+    }
+    folded_bsr_matrix_indices.push_back(folded_index);
+    uint32_t height_of_folded_matrix = Rt * nnz_rows;
+
+    uint32_t num_blocks_x = Nt / in1_block_w;
+    uint32_t num_blocks_y = nnz_rows;
+    uint32_t num_blocks_total = num_blocks_x * num_blocks_y;
+
+    uint32_t num_iters_x = (num_blocks_x + num_cores_x - 1) / num_cores_x;
+    uint32_t num_iters_y = (num_blocks_y + num_cores_y - 1) / num_cores_y;
+
+
+    uint32_t num_work_regions = (num_blocks_total + num_iters_x * num_iters_y - 1)/ (num_iters_x * num_iters_y);
+    uint32_t target_num_cores;
+    if (num_work_regions < num_cores_total)
+        target_num_cores = num_work_regions;
+    else
+        target_num_cores = num_cores_total;
+
+    uint32_t out_subblock_h = 0, out_subblock_w = 0;
+    for (auto& subblock_hw : bmm_op_utils::SUBBLOCK_HW_CHOICES) {
+        out_subblock_h = std::get<0>(subblock_hw);
+        out_subblock_w = std::get<1>(subblock_hw);
+        if (in0_block_h % out_subblock_h == 0 and in1_block_w % out_subblock_w == 0) {
+            break;
+        }
+    }
+
+    CoreCoord start_core = {0, 0};
+    // CoreCoord core_range = bmm_op_utils::get_core_range(num_blocks_y / num_iters_y, num_blocks_x / num_iters_x, num_cores_y, num_cores_x);
+    CoreCoord core_range(0, 0);
+    if ( (num_blocks_y / num_iters_y) <= num_cores_y &&
+        (num_blocks_x / num_iters_x) <= num_cores_x) {
+        core_range.x = (num_blocks_x + num_iters_x - 1) / num_iters_x;
+        core_range.y = (num_blocks_y + num_iters_y - 1) / num_iters_y;
+    }
+    uint32_t start_core_x = start_core.x;
+    uint32_t start_core_y = start_core.y;
+    uint32_t num_cores_c = core_range.x;
+    uint32_t num_cores_r = core_range.y;
+
+    // TODO: when only using one core, all_except_left_column is bad and throws a runtime error.
+    CoreRange all_cores(
+        {(std::size_t)start_core_x, (std::size_t)start_core_y},
+        {(std::size_t)start_core_x + num_cores_c - 1, (std::size_t)start_core_y + num_cores_r - 1});
+
+    CoreRange in0_injector_cores(
+        {(std::size_t)start_core_x, (std::size_t)start_core_y},
+        {(std::size_t)start_core_x, (std::size_t)start_core_y + num_cores_r - 1});
+
+
+    // this variable exists purely to allow the constructor to CoreRange to not fail when we only use one column of the core grid
+    uint32_t column_offset = num_cores_c > 1 ? num_cores_c : num_cores_c + 1;
+    CoreRange in0_receiver_cores(
+        {(std::size_t)start_core_x + 1, (std::size_t)start_core_y},
+        {(std::size_t)start_core_x + column_offset - 1, (std::size_t)start_core_y + num_cores_r - 1});
+    
+    // may not end up using these
+    CoreRange top_row(
+        {(std::size_t)start_core_x, (std::size_t)start_core_y},
+        {(std::size_t)start_core_x + num_cores_c - 1, (std::size_t)}start_core_y);
+
+    CoreRange all_but_top_row(
+        {(std::size_t)start_core_x, (std::size_t)start_core_y + 1},
+        {(std::size_t)start_core_x + column_offset - 1, (std::size_t)start_core_y + num_cores_r - 1});
+
+    
+    // TODO: double check the semaphore apis so you know 1. what all the functions do and 2. whether INVALID -- 0 and VALID -- 1.
+    auto in0_sender_semaphore_id = tt::tt_metal::CreateSemaphore(program, core_grid, INVALID);
+    auto in0_receiver_semaphore_id = tt::tt_metal::CreateSemaphore(program, core_grid, INVALID);
+    auto in0_valid_semaphore_id = tt::tt_metal::CreateSemaphore(program, core_grid, VALID);
+    auto in1_sender_semaphore_id = tt::tt_metal::CreateSemaphore(program, core_grid, INVALID);
+    auto in1_receiver_semaphore_id = tt::tt_metal::CreateSemaphore(program, core_grid, INVALID);
+    auto in1_valid_semaphore_id = tt::tt_metal::CreateSemaphore(program, core_grid, VALID);
+
+
+    // Circural Buffer sizing
+    uint32_t in0_CB_num_tiles = in0_block_h * in0_block_w * 2; // double buffer
+    uint32_t in0_CB_size = in0_CB_num_tiles * single_tile_size;
+    uint32_t in1_CB_num_tiles = in0_block_w * in1_block_w * 2; // double buffer
+    uint32_t in1_CB_size = in1_CB_num_tiles * single_tile_size;
+    uint32_t out_CB_num_tiles = in0_block_h * in1_block_w; // single buffer
+    uint32_t out_CB_size = out_CB_num_tiles * single_tile_size;
+
+    uint32_t in0_num_subblocks = (in0_block_h / out_subblock_h);
+    uint32_t in0_block_num_tiles = out_subblock_h * in0_block_w * in0_num_subblocks;
+    uint32_t in0_subblock_num_tiles = out_subblock_h * in0_block_w; // this is named weird but it's correct.
+
+    uint32_t in1_num_subblocks = (in1_block_w / out_subblock_w);
+    uint32_t in1_block_num_tiles = out_subblock_w * in0_block_w * in1_num_subblocks;
+    uint32_t in1_per_core_w = out_subblock_w * in1_num_subblocks;
+
+    uint32_t out_subblock_num_tiles = out_subblock_h * out_subblock_w;
+
+
+    // DRAM buffers initialiation
+    uint32_t dram_buffer_dst_row_size =
+        single_tile_size * Rt * Nt;
+
+    uint32_t dram_buffer_dst_total_size = dram_buffer_dst_row_size * nnz_rows;
+
+    uint32_t dram_buffer_A_size =
+        single_tile_size * Rt * Ct * nnz_blocks;  // num_tiles of FP16_B, hard-coded in the reader/writer kernels
+    uint32_t dram_buffer_B_size =
+        single_tile_size * Nt * Kt;  // num_tiles of FP16_B, hard-coded in the reader/writer kernels
+
+    uint32_t dram_buffer_col_indices_size =
+        sizeof(indexing_data_format) * nnz_blocks;
+    // Round up to tile size
+    dram_buffer_col_indices_size = indexing_data_single_tile_size * ((indexing_data_single_tile_size - 1 + dram_buffer_col_indices_size) / (indexing_data_single_tile_size));
+
+    uint32_t dram_buffer_indptr_size =
+        sizeof(indexing_data_format) * (M / R + 1);
+    // Round up to tile size
+    dram_buffer_indptr_size = indexing_data_single_tile_size * ((indexing_data_single_tile_size - 1 + dram_buffer_indptr_size) / (indexing_data_single_tile_size));
+
+    auto dst_dram_buffer = MakeBuffer(device, dram_buffer_dst_total_size, single_tile_size);
+    auto src0_dram_buffer = MakeBuffer(device, dram_buffer_A_size, single_tile_size);
+    auto src1_dram_buffer = MakeBuffer(device, dram_buffer_B_size, single_tile_size);
+    auto column_indices_dram_buffer = MakeBuffer(device, dram_buffer_col_indices_size, dram_buffer_col_indices_size);
+    auto indptr_dram_buffer = MakeBuffer(device, dram_buffer_indptr_size, dram_buffer_indptr_size);
+
+    /*
+    SRAM Circular Buffers
+    */
+    uint32_t src0_cb_index = CBIndex::c_0;  // 0
+    CircularBufferConfig cb_src0_config = CircularBufferConfig(in0_CB_size, {{src0_cb_index, cb_data_format}})
+                                              .set_page_size(src0_cb_index, single_tile_size);
+    auto cb_src0 = tt_metal::CreateCircularBuffer(program, all_cores, cb_src0_config);
+
+    uint32_t src1_cb_index = CBIndex::c_1;  // 1
+    CircularBufferConfig cb_src1_config = CircularBufferConfig(in1_CB_size, {{src1_cb_index, cb_data_format}})
+                                              .set_page_size(src1_cb_index, single_tile_size);
+    auto cb_src1 = tt_metal::CreateCircularBuffer(program, all_cores, cb_src1_config);
+
+    uint32_t output_cb_index = tt::CBIndex::c_16;
+    uint32_t interm0_cb_index = tt::CBIndex::c_24;
+    std::map<uint8_t, tt::DataFormat> output_cb_data_format_spec{
+        {output_cb_index, cb_data_format}, {interm0_cb_index, cb_data_format}};
+        CircularBufferConfig cb_output_config = CircularBufferConfig(out_CB_size, output_cb_data_format_spec)
+        .set_page_size(output_cb_index, single_tile_size)
+        .set_page_size(interm0_cb_index, single_tile_size);
+        auto cb_output = tt_metal::CreateCircularBuffer(program, all_cores, cb_output_config);
+
+    uint32_t column_indices_cb_index = CBIndex::c_2;  // 2
+    CircularBufferConfig cb_column_indices_config = CircularBufferConfig(
+        dram_buffer_col_indices_size, {{column_indices_cb_index, tt::DataFormat::Int32}})
+                                                .set_page_size(column_indices_cb_index, dram_buffer_col_indices_size);
+    auto cb_column_indices = tt_metal::CreateCircularBuffer(program, all_cores, cb_column_indices_config);
+
+    auto indptr_cb_index = CBIndex::c_3; // 3
+    auto cb_indptr = MakeCircularBuffer(program, all_cores, indptr_cb_index, dram_buffer_indptr_size, dram_buffer_indptr_size, indexing_data_format);
+
+    
+    /* 
+        Compile-time arguments.
+
+        Start answering questions about what you want your two DM kernels to do.
+        Start coding. 
+
+        Decision: follow their example, both kernels read, one writes.
+        TODO: only one kernel should read the indexing data from DRAM. 
+            - the kernel to read the indexing data should use a semaphore to let the other kernel know it's ready?
+        
+
+    */
+}
+}
